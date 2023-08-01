9dd1d720eca6f3db0bac145451af82111033b50a
DavitGyogchyan <mikayel1999@mail.com>
2023-08-01 00:15:08 +0000

    first commit, simple excercise

--- src/App.js
+++ src/App.js
@@ -4,62 +4,62 @@ import "./style.css";
 export default function App() {
   return (
     <div className="App">
-      <FlashCards />
+      <Counter />
     </div>
   );
 }
 
-const questions = [
-  {
-    id: 3457,
-    question: "What language is React based on?",
-    answer: "JavaScript",
-  },
-  {
-    id: 7336,
-    question: "What are the building blocks of React apps?",
-    answer: "Components",
-  },
-  {
-    id: 8832,
-    question: "What's the name of the syntax we use to describe a UI in React?",
-    answer: "JSX",
-  },
-  {
-    id: 1297,
-    question: "How to pass data from parent to child components?",
-    answer: "Props",
-  },
-  {
-    id: 9103,
-    question: "How to give components memory?",
-    answer: "useState hook",
-  },
-  {
-    id: 2002,
-    question:
-      "What do we call an input element that is completely synchronised with state?",
-    answer: "Controlled element",
-  },
-];
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
 
-function FlashCards() {
-  const [selectdId, setSelectedId] = useState(null)
-
-  function handlerClick(id) {
-    setSelectedId(id !== selectdId ? id : null)
+  function handleReset() {
+    setCount(0);
+    setStep(1);
   }
 
+  const date = new Date("August 01 2023");
+  date.setDate(date.getDate() + count);
+
   return (
-    <div className="flashcards">
-      {questions.map((question) => (
-        <div 
-        key={question.id} 
-        onClick={() => handlerClick(question.id)}
-        className= {question.id === selectdId ? 'selected' : ''}>
-          <p>{question.id === selectdId ? question.answer : question.question}</p>
+    <div>
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span>Step: {step}</span>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
         </div>
-      ))}
+      ) : null}
     </div>
   );
 }
